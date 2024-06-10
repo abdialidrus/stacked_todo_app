a2e55aa961566a2b7f43823586c9a124782d8817
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2024-06-10 12:21:38 +0700

    delete home view

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,5 @@
 import 'package:stacked_todo_app/ui/bottom_sheets/notice/notice_sheet.dart';
 import 'package:stacked_todo_app/ui/dialogs/info_alert/info_alert_dialog.dart';
-import 'package:stacked_todo_app/ui/views/home/home_view.dart';
 import 'package:stacked_todo_app/ui/views/startup/startup_view.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
@@ -11,7 +10,6 @@ import 'package:stacked_todo_app/services/hive_service.dart';
 
 @StackedApp(
   routes: [
-    MaterialRoute(page: HomeView),
     MaterialRoute(page: StartupView),
     MaterialRoute(page: TodoView),
 // @stacked-route
